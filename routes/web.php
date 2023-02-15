84bdcb37821fefd7c28a30bbf7152d668abcd246
ceronGithub <developerceron@gmail.com>
2023-02-15 11:06:44 +0800

    Fea: modi-webRoute

--- routes/web.php
+++ routes/web.php
@@ -21,26 +21,26 @@ Route::get('/', function () {
 Auth::routes();
 //via modal
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/companyViaModal', [App\Http\Controllers\CompanyViaModalController::class, 'index'])->name('companyModalVer');
-Route::post('/companyViaModal/store', [App\Http\Controllers\CompanyViaModalController::class, 'addNewCompany'])->name('addNewCompanyModalVer');
-Route::post('/companyViaModal/delete', [App\Http\Controllers\CompanyViaModalController::class, 'delete'])->name('deleteCompanyModalVer');
-Route::post('/companyViaModal/update', [App\Http\Controllers\CompanyViaModalController::class, 'update'])->name('updateCompanyModalVer');
+Route::get('/companyViaModal', [App\Http\Controllers\modal\CompanyViaModalController::class, 'index'])->name('companyModalVer');
+Route::post('/companyViaModal/store', [App\Http\Controllers\modal\CompanyViaModalController::class, 'addNewCompany'])->name('addNewCompanyModalVer');
+Route::post('/companyViaModal/delete', [App\Http\Controllers\modal\CompanyViaModalController::class, 'delete'])->name('deleteCompanyModalVer');
+Route::post('/companyViaModal/update', [App\Http\Controllers\modal\CompanyViaModalController::class, 'update'])->name('updateCompanyModalVer');
 
 
 //via next page
-Route::get('/companyViaNextPage', [App\Http\Controllers\CompanyViaNextpageController::class, 'index'])->name('companyNextPageVer');
+Route::get('/companyViaNextPage', [App\Http\Controllers\nextpage\CompanyViaNextpageController::class, 'index'])->name('companyNextPageVer');
 //----------- add start
-Route::get('/companyViaNextPage/NewRecordPage', [App\Http\Controllers\CompanyViaNextpageController::class, 'addPage'])->name('companyNextPageAddVer');
-Route::get('/companyViaNextPage/storeFunc', [App\Http\Controllers\CompanyViaNextpageController::class, 'store'])->name('companyNextPageAddFuncVer');
+Route::get('/companyViaNextPage/NewRecordPage', [App\Http\Controllers\nextpage\CompanyViaNextpageController::class, 'addPage'])->name('companyNextPageAddVer');
+Route::get('/companyViaNextPage/storeFunc', [App\Http\Controllers\nextpage\CompanyViaNextpageController::class, 'store'])->name('companyNextPageAddFuncVer');
 //----------- add end
 //----------- view start
-Route::get('/companyViaNextPage/ViewCompanyDetails/{id}', [App\Http\Controllers\CompanyViaNextpageController::class, 'view'])->name('companyNextPageViewVer');
+Route::get('/companyViaNextPage/ViewCompanyDetails/{id}', [App\Http\Controllers\nextpage\CompanyViaNextpageController::class, 'view'])->name('companyNextPageViewVer');
 //----------- view end
 //----------- edit start
-Route::get('/companyViaNextPage/EditPage/{id}', [App\Http\Controllers\CompanyViaNextpageController::class, 'editPage'])->name('companyNextPageEditVer');
-Route::get('/companyViaNextPage/editFunc', [App\Http\Controllers\CompanyViaNextpageController::class, 'edit'])->name('companyNextPageEditFuncVer');
+Route::get('/companyViaNextPage/EditPage/{id}', [App\Http\Controllers\nextpage\CompanyViaNextpageController::class, 'editPage'])->name('companyNextPageEditVer');
+Route::get('/companyViaNextPage/editFunc', [App\Http\Controllers\nextpage\CompanyViaNextpageController::class, 'edit'])->name('companyNextPageEditFuncVer');
 //----------- edit end
 //----------- delete start
-Route::get('/companyViaNextPage/DeletePage/{id}', [App\Http\Controllers\CompanyViaNextpageController::class, 'deletePage'])->name('companyNextPageDeleteVer');
-Route::get('/companyViaNextPage/deleteFunc', [App\Http\Controllers\CompanyViaNextpageController::class, 'delete'])->name('companyNextPageDeleteFuncVer');
+Route::get('/companyViaNextPage/DeletePage/{id}', [App\Http\Controllers\nextpage\CompanyViaNextpageController::class, 'deletePage'])->name('companyNextPageDeleteVer');
+Route::get('/companyViaNextPage/deleteFunc', [App\Http\Controllers\nextpage\CompanyViaNextpageController::class, 'delete'])->name('companyNextPageDeleteFuncVer');
 //----------- delete end
