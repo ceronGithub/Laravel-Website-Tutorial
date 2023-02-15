ad8775c553bd1ac1f2d40754838fe9ce75c6fa53
ceronGithub <developerceron@gmail.com>
2023-02-15 16:03:52 +0800

    fea: modi-productmigration

--- database/migrations/2023_02_15_064457_create_products_table.php
+++ database/migrations/2023_02_15_064457_create_products_table.php
@@ -15,6 +15,9 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('company_id');
+            $table->string('product_name');
+            $table->string('product_name');
             $table->timestamps();
         });
     }
